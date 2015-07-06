65e6f9d3018e4181a46b862216ea1000c5c7736c
Juan Elosua <juan.elosua@gmail.com>
2015-07-06 20:41:45 -0300

    refactor and close #6

---
+++ webapp/js/app/permalink.js
@@ -0,0 +1,25 @@
+define(['app/context'], function(context){
+
+    function set(){
+        console.log(context);
+        var ctxt = JSON.stringify(context);
+        ctxt = ctxt.replace(/\"|\{|\}/g, '').replace(/,/g, "&").replace(/:/g, "=")
+        location.hash = ctxt;
+    }
+
+    function get(){
+        var u = location.hash.replace("#", "");
+        if(u){
+            u = u.split(/\&/);
+            u.forEach(function(c, i){
+                c = c.split("=");
+                context[c[0]] = eval(c[1]);
+            });
+        }
+    }
+
+    return {
+        get: get,
+        set: set
+    };
+});
